7b674ba04fd1057f85cbee4e3b9a78dababf8f7d
dearfl <i@flr.me>
2025-11-08 17:53:07 +0800

    add vaultix and v2ray

--- hosts/vultr/vaultix.nix
+++ hosts/vultr/vaultix.nix
@@ -2,7 +2,11 @@ _: {
   config = {
     vaultix = {
       settings = {
+<<<<<<< HEAD
         hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDqt2q+BzxrPzB3/sjdwoL+hflATcV5wypvCCeCubmZB";
+=======
+        hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIP8Je+ds47siDRtiHhT6ko/SwnX+2reYv+A/B8GwZrY";
+>>>>>>> 94baaf5 (add vaultix and v2ray)
       };
     };
   };
